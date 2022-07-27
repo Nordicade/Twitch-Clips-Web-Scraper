03b3bf91238eff6941c68c73e43cae83a5a8ea4a
Nicholas Nord <Nicholasnord@gmail.com>
2022-07-27 11:30:11 -0700

    Got the clean download url

--- TwitchDownloader.js
+++ TwitchDownloader.js
@@ -4,12 +4,33 @@ const Constants = {
   DOWNLOAD_LINK_REGEX: /(https:\/\/clips\.twitch\.tv\/embed\?clip=)(.*?)(')/gm,
   TITLE_REGEX: /(alt=")(.*?)(")/gm,
   TWITCH_DOMAIN: 'https://clips.twitch.tv/embed?clip=',
+  TWITCH_BASE: 'https://clips.twitch.tv/',
   STREAMS_CHARTS_URL: 'https://streamscharts.com/clips',
   DOWNLOAD_CLIP_URL: 'https://streamscharts.com/clips/downloader',
 }
 
 function matchTwitchDomain(htmlBlock){
   return htmlBlock.indexOf(Constants.TWITCH_DOMAIN) > 0;
+}
+
+function matchClip(htmlBlock){
+  let starting = 0;
+  let ending = 0;
+
+  if (htmlBlock.indexOf(Constants.TWITCH_DOMAIN) > 0){
+    starting = htmlBlock.indexOf(Constants.TWITCH_DOMAIN) + Constants.TWITCH_DOMAIN.length;
+  }
+  else {
+    console.log('reee');
+  }
+  let newHtmlBlock = htmlBlock.substring(starting, htmlBlock.length);
+  if (newHtmlBlock.indexOf('\'') > 0){
+    ending = newHtmlBlock.indexOf('\'');
+  }
+  else {
+    console.log('reee 2 ');
+  }
+  return substring = newHtmlBlock.substring(0 ,ending);
 }
 
 function matchClipURL(htmlBlock){
@@ -41,6 +62,7 @@ for(let i = 0; i < clipHTML.length - 1; i++){
   // const url = matchClipURL(clipHTML[i]);
   // const title = matchClipTitle(clipHTML[i]);
   // console.log(`${i} : ${url} - ${title}`);
-  const result = matchTwitchDomain(clipHTML[i])
+  const result = Constants.TWITCH_BASE + matchClip(clipHTML[i])
+  console.log(result);
 }
 console.log(" - - exiting Twitch Downloader - -");
